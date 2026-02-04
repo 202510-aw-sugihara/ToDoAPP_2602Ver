0891171b2cf47a2ecf6f8ad38b2d0ab7c42febbc
202510-aw-sugihara <202510_aw_sugihara@x-ide.sakura.ne.jp>
2026-02-04 11:11:09 +0900

    画面作成（リンク遷移）

---
+++ todo/src/main/java/com/example/todo/TodoController.java
@@ -0,0 +1,27 @@
+package com.example.todo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class TodoController {
+
+  // ToDo一覧画面を表示します。
+  @GetMapping("/todos")
+  public String list() {
+    return "todo/list";
+  }
+
+  // ToDo新規作成画面を表示します。
+  @GetMapping("/todos/new")
+  public String newTodo() {
+    return "todo/new";
+  }
+
+  // 指定IDのToDo詳細画面を表示します。
+  @GetMapping("/todos/{id}")
+  public String detail(@PathVariable("id") long id) {
+    return "todo/detail";
+  }
+}
